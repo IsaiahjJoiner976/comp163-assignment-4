a41ad266c4a9a3c11e2bfef332696887891de9bb
IsaiahjJoiner976 <ijjoiner@aggies.ncat.edu>
2025-09-25 14:49:40 -0400

    "Pass Test Case 1: Initial game setup with required variables"

--- isaiah_assignment_4.py
+++ isaiah_assignment_4.py
@@ -4,3 +4,5 @@ study_hours = int(23)
 social_points = int(60)
 stress_level = int(50)
 
+print(f"Welcome {student_name}!")
+print(f"Starting Stats → GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress Level: {stress_level}")
